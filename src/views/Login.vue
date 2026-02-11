37e777bb86d10dc2da26c4568b47bde6a0528a47
Navjot Koth <navjotkoth@gmail.com>
2026-02-11 18:21:26 +0530

    Update Login.vue

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -38,7 +38,7 @@ export default {
   methods: {
     async login() {
       try {
-        const response = await fetch('http://localhost:5000/login', {
+        const response = await fetch('/login', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json'
